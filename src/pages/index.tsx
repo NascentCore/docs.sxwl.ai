86d6aeb00924e7deefb362419bb79d00a3c1179d
cairong-ai <146914987+cairong-ai@users.noreply.github.com>
2023-11-11 13:41:05 +0800

    Add computer power user guide (#2)
    * add computer power user guide
    
    * update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="/docs/user-guide"
           >
             使用指南
           </Link>
