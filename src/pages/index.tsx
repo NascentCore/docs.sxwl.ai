0df0dda7f7bc96799f7b0ca9e1eb6505be5821e1
zhoujie <zhoujie16m@163.com>
2023-12-09 01:06:27 +0800

    修改兼容移动端样式

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx(styles.heroBanner)}>
-      <div className="container" style={{ height: 200, paddingTop: 100 }}>
+      <div className={styles.container}>
         <Heading as="h1" className="hero__title" style={{ fontSize: 35 }}>
           算想云 Serverless 大模型训推云服务
         </Heading>
@@ -23,15 +23,12 @@ function HomepageHeader() {
           </Link>
         </div>
       </div>
-      <div className="container" style={{ height: 200, paddingTop: 100 }}>
+      <div className={styles.container}>
         <Heading as="h1" className="hero__title" style={{ fontSize: 35 }}>
           三千平台：私有化部署云原生大模型训推软件平台
         </Heading>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/3k"
-          >
+          <Link className="button button--secondary button--lg" to="/docs/3k">
             三千平台使用指南
           </Link>
         </div>
